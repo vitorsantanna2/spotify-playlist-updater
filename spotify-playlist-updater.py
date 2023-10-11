5482a643934850b771269a4a06cf8e353912d9c4
Vitor Santanna <95882160+vitorsantanna2@users.noreply.github.com>
2023-10-11 01:10:54 -0300

    Update spotify-playlist-updater.py

--- spotify-playlist-updater.py
+++ spotify-playlist-updater.py
@@ -9,25 +9,21 @@ from spotipy.oauth2 import SpotifyOAuth
 import base64
 with open("./data/photo.png", "rb") as img_file:
     myimage = base64.b64encode(img_file.read())
-
 load_dotenv()
-
 print("Starting Playlist updater")
-
-@repeat(every(30).seconds)
+@repeat(every(1).minutes)
 def func():
     scope = 'playlist-modify-public ugc-image-upload'
 
     username = os.environ['USER_SPOTIFY']
-    # Spotify Developer App Client ID and Secret ID
     SPOTIPY_CLIENT_ID = os.environ['CLIENT_ID']
     SPOTIPY_CLIENT_SECRET = os.environ['CLIENT_SECRET']
     redirect_uri = os.environ['REDIRECT_URI']
-    id = os.environ['PLAYLIST_ID']
     token = util.prompt_for_user_token(username,scope,SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET,redirect_uri)
     sp = spotipy.Spotify(auth=token)
 
     # Playlist Data
+    id = os.environ['PLAYLIST_ID']
     playlist_name = 'new_playlist'
     playlist_description = 'playlist_one'
     image_b64 = myimage
